560aab52714b458d968cfbb943b9654e3ba8c5fc
Wilhelm Wie-Groenhof <wilhelwi@stud.ntnu.no>
2023-09-12 15:47:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import classes as cs
 g = 9.81
 m_centroid = 0.5
 m_rotor = 0.1
+d = 0.1
 C_q = 1 #drag_coefficient
 C_t = 1 #Thrust coefficient
 
@@ -13,10 +14,11 @@ C_t = 1 #Thrust coefficient
 def main():
     rotors = []
 
-    rotors.append(cs.Rotor(m_rotor,[0,0,0],[0.1,0,0],C_q,C_t))
-    rotors.append(cs.Rotor(m_rotor,[0,0,0],[0,0.1,0],C_q,C_t))
-    rotors.append(cs.Rotor(m_rotor,[0,0,0],[-0.1,0,0],C_q,C_t))
-    rotors.append(cs.Rotor(m_rotor,[0,0,0],[0,-0.1,0],C_q,C_t))
+    #Normal Quadrotor rotors
+    rotors.append(cs.Rotor(m_rotor,[0,0,0],[d,0,0],C_q,C_t))
+    rotors.append(cs.Rotor(m_rotor,[0,0,0],[0,d,0],C_q,C_t))
+    rotors.append(cs.Rotor(m_rotor,[0,0,0],[-d,0,0],C_q,C_t))
+    rotors.append(cs.Rotor(m_rotor,[0,0,0],[0,-d,0],C_q,C_t))
     
     quad = cs.QuadRotor(m_centroid, rot_vec=[0,0,0],t_vec=[1,1,1], rotors=rotors)
 
