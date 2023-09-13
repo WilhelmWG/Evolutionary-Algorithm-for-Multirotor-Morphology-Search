4e62c101c8bb1752968d7e6504301f2f857486b0
Wilhelm Wie-Groenhof <wilhelwi@stud.ntnu.no>
2023-09-13 17:15:00 +0200

    Forward Euler Attempted

--- QuadrotorDynamics.py
+++ QuadrotorDynamics.py
@@ -3,9 +3,14 @@ import numpy as np
 import scipy as sp
 from typing import List, Tuple
 from scipy.spatial.transform import Rotation as R
+from scipy.integrate import solve_ivp
 
 
-
+g = 9.81
+def skew(x):
+    return np.array([[0, -x[2], x[1]],
+                     [x[2], 0, -x[0]],
+                     [-x[1], x[0], 0]])
 
 class Limb:
     def __init__(self, m, rot_vec, t_vec):
@@ -41,13 +46,23 @@ class DepthCamera(Limb):
         self.AoV = AoV
 
 class QuadRotor:
-    def __init__(self, m, rot_vec, t_vec, rotors: List[Rotor], dep_cams: List[DepthCamera], IMU: IMU):
+    def __init__(self, m, rot_vec, t_vec, ang_vel, rotors: List[Rotor], dep_cams: List[DepthCamera], IMU: IMU):
+        self.time = 0
         self.m = m
+        
         self.rot_vec = rot_vec #
+        self.rot_vec_dot = np.zeros((3,))
         self.t_vec = t_vec
+        self.t_vec_dot = np.zeros((3,))
+        self.ang_vel = ang_vel
+        self.ang_vel_dot = np.zeros((3,))
+        
         self.rotors = rotors
         self.dep_cams = dep_cams
         self.IMU = IMU
+        self.J = self.calculate_inertial_tensor()
+        
+    
     def calculate_inertial_tensor(self):
         J = np.zeros((3,3),dtype=float)
         for rotor in self.rotors:
@@ -61,16 +76,50 @@ class QuadRotor:
         t_vec = np.reshape(self.IMU.t_vec,(3,1))
         J += self.IMU.m*(sp.linalg.norm(t_vec)**2*np.eye(3)-(t_vec)@(t_vec.T))
         
-        return J
+        self.J = J
     
-    def calculate_sum_of_forces(self):
+    def calculate_sum_of_forces_bf(self):
         sum_force = np.zeros((3,))
         for rotor in self.rotors:
             sum_force += np.reshape(rotor.get_force_bf(),(3,))
+        rot_mat = R.from_euler("zxy",self.rot_vec)
+        rot_mat = rot_mat.as_matrix()
+        sum_force -= np.reshape(rot_mat@rotor.get_force_bf(),(3,))
         return sum_force
 
-    def calculate_torque_from_thrust(self):
+    def calculate_torque_from_thrust_bf(self):
         sum_torque = np.zeros((3,))
         for rotor in self.rotors:
             sum_torque += np.cross(rotor.t_vec,np.reshape(rotor.get_force_bf(),(3,)))
         return sum_torque
+    
+    def calculate_reaction_torque_bf(self):
+        sum_torque = np.zeros((3,))
+        for rotor in self.rotors:
+            sum_torque += np.reshape(rotor.get_force_bf()*rotor.C_q/rotor.C_t,(3,))*np.sign(rotor.rps)
+        return sum_torque
+    
+    def calculate_sum_of_torques_bf(self):
+        return self.calculate_reaction_torque_bf()+self.calculate_torque_from_thrust_bf()
+
+    def simulate_timestep(self,delta_t):
+        rot_mat = R.from_euler("zxy",self.rot_vec)
+        rot_mat = rot_mat.as_matrix()
+        
+        
+        
+        #position
+        self.t_vec += self.t_vec_dot*delta_t #1a)
+        self.rot_vec += self.rot_vec_dot*delta_t
+        
+        #Angular velocity
+        self.ang_vel += self.ang_vel_dot*delta_t
+        
+        #velocity
+        self.ang_vel_dot = -np.linalg.inv(self.J)@(np.cross(self.ang_vel,self.J@self.ang_vel)+self.calculate_sum_of_torques_bf()) #SUPA SLOW, MAKE IT LINEAR SOLVE INSTEAD
+        self.t_vec_dot += rot_mat@self.calculate_sum_of_forces_bf()*delta_t #1b)
+        R_dot = rot_mat@skew(self.ang_vel)
+        rot_dot = R.from_matrix(R_dot)
+        self.rot_vec_dot = rot_dot.as_euler("zxy")*delta_t
+        
+        self.time += delta_t
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # import control as ct
 import numpy as np
-import classes as cs
+import QuadrotorDynamics as qd
 
 g = 9.81
 m_centroid = 0.5
@@ -16,18 +16,22 @@ C_t = 0.01 #Thrust coefficient
 def main():
     rotors = []
     dep_cams = []
-    IMU = cs.IMU(m_IMU,np.array([0,0,0],dtype=float),np.array([0,0,0],dtype=float))
+    IMU = qd.IMU(m_IMU,np.array([0,0,0],dtype=float),np.array([0,0,0],dtype=float))
     #Normal Quadrotor rotors
-    rotors.append(cs.Rotor(m_rotor,np.array([0,0,0],dtype=float),np.array([d,0,0],dtype=float),2,C_q,C_t))
-    rotors.append(cs.Rotor(m_rotor,np.array([0,0,0],dtype=float),np.array([0,d,0],dtype=float),30,C_q,C_t))
-    rotors.append(cs.Rotor(m_rotor,np.array([0,0,0],dtype=float),np.array([-d,0,0],dtype=float),2,C_q,C_t))
-    rotors.append(cs.Rotor(m_rotor,np.array([0,0,0],dtype=float),np.array([0,-d,0],dtype=float),20,C_q,C_t))
-    dep_cams.append(cs.DepthCamera(m_dep_cam, rot_vec=np.array([0,0,0],dtype=float), t_vec=np.array([0,0,0],dtype=float),AoV=40))
+    rotors.append(qd.Rotor(m_rotor,np.array([0,0,0],dtype=float),np.array([d,0,0],dtype=float),-10,C_q,C_t))
+    rotors.append(qd.Rotor(m_rotor,np.array([0,0,0],dtype=float),np.array([0,d,0],dtype=float),10,C_q,C_t))
+    rotors.append(qd.Rotor(m_rotor,np.array([0,0,0],dtype=float),np.array([-d,0,0],dtype=float),-10,C_q,C_t))
+    rotors.append(qd.Rotor(m_rotor,np.array([0,0,0],dtype=float),np.array([0,-d,0],dtype=float),10,C_q,C_t))
+    dep_cams.append(qd.DepthCamera(m_dep_cam, rot_vec=np.array([0,0,0],dtype=float), t_vec=np.array([0,0,0],dtype=float),AoV=40))
     
-    quad = cs.QuadRotor(m_centroid, rot_vec=[0,0,0],t_vec=[1,1,1], rotors=rotors, dep_cams = dep_cams, IMU = IMU)
-    print(quad.calculate_inertial_tensor())
-    print(quad.calculate_sum_of_forces())
-    print(quad.calculate_torque_from_thrust())
+    quad = qd.QuadRotor(m_centroid, rot_vec=np.array([0,0,0],dtype=float),t_vec=np.array([1,1,1],dtype=float), ang_vel=np.array([0,0,0],dtype=float), rotors=rotors, dep_cams = dep_cams, IMU = IMU)
+    print(f"Quadrotor Inertial Tensor: \n{quad.calculate_inertial_tensor()}")
+    print(f"Sum Of Forces: {quad.calculate_sum_of_forces_bf()}")
+    print(f"Torque From Thrust: {quad.calculate_torque_from_thrust_bf()}")
+    print(f"Reaction Torque: {quad.calculate_reaction_torque_bf()}")
+    for i in range(100):
+        quad.simulate_timestep(0.01)
+    breakpoint()
 
 if __name__ == "__main__":
     main()
