a221738370ef9469b31b24ca3750362c3690ab51
Wilhelm Wie-Groenhof <wilhelwi@stud.ntnu.no>
2023-09-13 15:06:24 +0200

    Inertial Tensor, Sum of Force, Sum of Torque

--- classes.py
+++ classes.py
@@ -1,13 +1,11 @@
-import control as ct
+# import control as ct
 import numpy as np
 import scipy as sp
+from typing import List, Tuple
 from scipy.spatial.transform import Rotation as R
-class QuadRotor:
-    def __init__(self, m, rot_vec, t_vec, rotors):
-        self.m = m
-        self.rot_vec = rot_vec #
-        self.t_vec = t_vec
-        self.rotors = rotors
+
+
+
 
 class Limb:
     def __init__(self, m, rot_vec, t_vec):
@@ -16,7 +14,7 @@ class Limb:
         self.t_vec = t_vec
 
 class Rotor(Limb):
-    def __init__(self,m,rot_vec,t_vec, rps, C_q, C_t):
+    def __init__(self, m, rot_vec, t_vec, rps, C_q, C_t):
         super().__init__(m,rot_vec,t_vec)
         self.rps = rps #Rotations per second
         self.C_q = C_q
@@ -24,16 +22,55 @@ class Rotor(Limb):
     #returns the force in body frame
     def get_force_bf(self):
         force_rf = self.C_t*self.rps**2*np.array([0,0,1])
+        force_rf = np.reshape(force_rf,(3,1))
         rot_mat = R.from_euler("zxy",self.rot_vec)
+        rot_mat = rot_mat.as_matrix()
         force_bf = rot_mat@force_rf
         return force_bf
     
-    def reaction_torque_bf(self):
+    def get_torque_bf(self):
         return
-    
-    
 
 class IMU(Limb):
     def __init__(self,m,rot_vec,t_vec):
         super().__init__(m,rot_vec,t_vec)
+
+class DepthCamera(Limb):
+    def __init__(self,m,rot_vec,t_vec,AoV):
+        super().__init__(m,rot_vec,t_vec)
+        self.AoV = AoV
+
+class QuadRotor:
+    def __init__(self, m, rot_vec, t_vec, rotors: List[Rotor], dep_cams: List[DepthCamera], IMU: IMU):
+        self.m = m
+        self.rot_vec = rot_vec #
+        self.t_vec = t_vec
+        self.rotors = rotors
+        self.dep_cams = dep_cams
+        self.IMU = IMU
+    def calculate_inertial_tensor(self):
+        J = np.zeros((3,3),dtype=float)
+        for rotor in self.rotors:
+            t_vec = np.reshape(rotor.t_vec,(3,1))
+            J += rotor.m*(sp.linalg.norm(t_vec)**2*np.eye(3)-(t_vec)@(t_vec.T))
+            
+        for dep_cam in self.dep_cams:
+            t_vec = np.reshape(dep_cam.t_vec,(3,1))
+            J += dep_cam.m*(sp.linalg.norm(t_vec)**2*np.eye(3)-(t_vec)@(t_vec.T))
         
+        t_vec = np.reshape(self.IMU.t_vec,(3,1))
+        J += self.IMU.m*(sp.linalg.norm(t_vec)**2*np.eye(3)-(t_vec)@(t_vec.T))
+        
+        return J
+    
+    def calculate_sum_of_forces(self):
+        sum_force = np.zeros((3,))
+        for rotor in self.rotors:
+            sum_force += np.reshape(rotor.get_force_bf(),(3,))
+        return sum_force
+
+    def calculate_torque_from_thrust(self):
+        sum_torque = np.zeros((3,))
+        for rotor in self.rotors:
+            sum_torque += np.cross(rotor.t_vec,np.reshape(rotor.get_force_bf(),(3,)))
+        return sum_torque
--- main.py
+++ main.py
@@ -1,28 +1,33 @@
-import control as ct
+# import control as ct
 import numpy as np
 import classes as cs
 
 g = 9.81
 m_centroid = 0.5
 m_rotor = 0.1
+m_IMU = 0.1
+m_dep_cam = 0.1
 d = 0.1
-C_q = 1 #drag_coefficient
-C_t = 1 #Thrust coefficient
+C_q = 0.001 #drag_coefficient
+C_t = 0.01 #Thrust coefficient
 
 
 
 def main():
     rotors = []
-
+    dep_cams = []
+    IMU = cs.IMU(m_IMU,np.array([0,0,0],dtype=float),np.array([0,0,0],dtype=float))
     #Normal Quadrotor rotors
-    rotors.append(cs.Rotor(m_rotor,[0,0,0],[d,0,0],C_q,C_t))
-    rotors.append(cs.Rotor(m_rotor,[0,0,0],[0,d,0],C_q,C_t))
-    rotors.append(cs.Rotor(m_rotor,[0,0,0],[-d,0,0],C_q,C_t))
-    rotors.append(cs.Rotor(m_rotor,[0,0,0],[0,-d,0],C_q,C_t))
+    rotors.append(cs.Rotor(m_rotor,np.array([0,0,0],dtype=float),np.array([d,0,0],dtype=float),2,C_q,C_t))
+    rotors.append(cs.Rotor(m_rotor,np.array([0,0,0],dtype=float),np.array([0,d,0],dtype=float),30,C_q,C_t))
+    rotors.append(cs.Rotor(m_rotor,np.array([0,0,0],dtype=float),np.array([-d,0,0],dtype=float),2,C_q,C_t))
+    rotors.append(cs.Rotor(m_rotor,np.array([0,0,0],dtype=float),np.array([0,-d,0],dtype=float),20,C_q,C_t))
+    dep_cams.append(cs.DepthCamera(m_dep_cam, rot_vec=np.array([0,0,0],dtype=float), t_vec=np.array([0,0,0],dtype=float),AoV=40))
     
-    quad = cs.QuadRotor(m_centroid, rot_vec=[0,0,0],t_vec=[1,1,1], rotors=rotors)
-
-    return
+    quad = cs.QuadRotor(m_centroid, rot_vec=[0,0,0],t_vec=[1,1,1], rotors=rotors, dep_cams = dep_cams, IMU = IMU)
+    print(quad.calculate_inertial_tensor())
+    print(quad.calculate_sum_of_forces())
+    print(quad.calculate_torque_from_thrust())
 
 if __name__ == "__main__":
     main()
